c6a82255c2e07d54c6077e266914e007f086b32d
Cesar Gutierrez Espitia <cesar.gutierrez.esp@gmail.com>
2021-04-22 22:55:59 -0500

    Se agrega pantalla de inicio se sesion, pantalla de registro y listado en dashboard

---
+++ app/containers/SingIn/styles.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+import * as theme from '../../theme';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    backgroundColor: theme.backgroundColor,
+  },
+  text: {
+    color: theme.brandColor000,
+    textAlign: 'center',
+  }
+});
+
+export default styles;
